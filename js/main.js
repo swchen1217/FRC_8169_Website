c59763a961602ffe881f2fe6252bfb8b0d2f76a2
Sz-Wei Chen <swchen1217@gmail.com>
2021-08-07 12:50:28 +0800

    Modify Layout, About Us

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@
     duration: 800,
 });*/
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function() {
+    $(".navbar-burger").click(function () {
 
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         $(".navbar-burger").toggleClass("is-active");
