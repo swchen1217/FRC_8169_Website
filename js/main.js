34765aac99963a737d32aad5a75f25dee574006a
Sz-Wei Chen <swchen1217@gmail.com>
2021-08-09 00:52:35 +0800

    Add Daily

--- js/main.js
+++ js/main.js
@@ -3,8 +3,9 @@
     duration: 800,
 });*/
 
-$(document).ready(function () {
+var hackmd_url = "https://api.allorigins.win/raw?url=https://hackmd.io/api/@FRC-8169/overview";
 
+$(document).ready(function () {
     // Check for click events on the navbar burger icon
     $(".navbar-burger").click(function () {
 
@@ -13,6 +14,14 @@ $(document).ready(function () {
         $(".navbar-menu").toggleClass("is-active");
 
     });
+
+    fetch(`${hackmd_url}?nocache=${Date.now()}`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            renderDaily(data)
+        });
 });
 
 window.onscroll = function () {
@@ -36,3 +45,50 @@ $("a[href^='#']").on("click", function (event) {
         500
     );
 });
+
+var member_tag_colors = {
+    "機構組": "is-primary",
+    "繪圖組": "is-success",
+    "電機組": "is-danger",
+    "程式組": "is-warning",
+    "公關組": "is-link",
+    "Driver Team": "is-orange",
+}
+
+function renderDaily(data) {
+    let html = "";
+    data['notes'].forEach((item) => {
+        let isDaily = false;
+        let tagsHtml = "";
+        item['tags'].forEach((tag) => {
+            if (tag == 'daily')
+                isDaily = true;
+            if (tag.startsWith('*')) {
+                tagsHtml += `<span class="tag ${member_tag_colors[tag.substr(1)] ?? 'is-info'}">${escapeHtml(tag.substr(1))}</span>`;
+            }
+        })
+        if (!isDaily)
+            return;
+        html += `<div class="column is-3">
+              <div class="card has-shadow-black has-radius-15">
+                <div class="card-content">
+                  <div class="media has-text-centered mb-4">
+                    <div class="media-content"><a class="title is-4" href="https://hackmd.io/${item['shortId']}" target="_blank">${escapeHtml(item['title'])}</a><br>
+                      <p class="subtitle is-6">Published At ${new Date(item['publishedAt']).toLocaleDateString()}</p>
+                    </div>
+                  </div>
+                  <div class="content">
+                    <div class="tags are-medium is-centered">${tagsHtml}</div>
+                  </div>
+                </div>
+              </div>
+            </div>`;
+    });
+    document.getElementById('dailyCard').innerHTML = html;
+}
+
+function escapeHtml(string) {
+    return String(string).replace(/[&<>"'\/]/g, function (s) {
+        return entityMap[s];
+    });
+}
